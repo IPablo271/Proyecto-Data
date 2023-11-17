92c0fb39af25f9ceac3975d90299df56836ff2ac
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2023-11-17 10:06:06 -0600

    agregando ultimas graficas
    Co-Authored-By: IPablo271 <69815580+IPablo271@users.noreply.github.com>
    Co-Authored-By: Andrés de la Roca <60155698+andresdlRoca@users.noreply.github.com>
    Co-Authored-By: Javier Mombiela <61723252+javim7@users.noreply.github.com>
    Co-Authored-By: Jun Woo Lee <60154594+jwlh00@users.noreply.github.com>

--- client/src/components/CardsCarousel/CardsCarousel.tsx
+++ client/src/components/CardsCarousel/CardsCarousel.tsx
@@ -2,7 +2,11 @@ import { Carousel } from '@mantine/carousel';
 import { Image } from '@mantine/core';
 
 import Modelo1accuracy from '../../images/Modelo1accuracy.png';
+import Modelo1 from '../../images/Modelo1.png';
+
 import Modelo2accuracy from '../../images/Modelo2accuracy.png';
+import Modelo2 from '../../images/Modelo2.png';
+
 
 
 
@@ -11,18 +15,33 @@ export function CardsCarousel() {
         <Carousel slideSize="20%" height={200} slideGap="md" loop>
             <Carousel.Slide>
                 <h1>Modelo 1</h1>
-                <Image
+                <Image style={{ width: '50%' }}
                     radius="md"
                     src={Modelo1accuracy}
                 />
             </Carousel.Slide>
             <Carousel.Slide>
+                <h1>Modelo 1</h1>
+                <Image style={{ width: '50%' }}
+                    radius="md"
+                    src={Modelo1}
+                />
+            </Carousel.Slide>
+            <Carousel.Slide>
                 <h1>Modelo 2</h1>
-                <Image
+                <Image style={{ width: '50%' }}
                     radius="md"
                     src={Modelo2accuracy}
                 />
             </Carousel.Slide>
+            <Carousel.Slide>
+                <h1>Modelo 2</h1>
+                <Image style={{ width: '50%' }}
+                    radius="md"
+                    src={Modelo2}
+                />
+            </Carousel.Slide>
+
         </Carousel>
     );
 }
